9517ff34c6217ff2e00f823826b0dec29d49030e
Tyler Torres <tyler.john.torres@gmail.com>
2017-10-11 16:49:47 -0600

    ..Working

---
+++ src/components/App.js
@@ -0,0 +1,127 @@
+import React, { Component } from 'react';
+import Toolbar from './Toolbar'
+import Messages from './Messages'
+import '../App.css';
+
+class App extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = { messages: props.messages }
+  }
+
+  toggleProperty(message, property) {
+    const index = this.state.messages.indexOf(message)
+    this.setState({
+      messages: [
+        ...this.state.messages.slice(0, index),
+        { ...message, [property]: !message[property] },
+        ...this.state.messages.slice(index + 1),
+      ]
+    })
+  }
+
+  toggleSelect(message) {
+    this.toggleProperty(message, 'selected')
+  }
+
+  toggleStar(message) {
+    this.toggleProperty(message, 'starred')
+  }
+
+  markAsRead() {
+    this.setState({
+      messages: this.state.messages.map(message => (
+        message.selected ? { ...message, read: true } : message
+      ))
+    })
+  }
+
+  markAsUnread() {
+    this.setState({
+      messages: this.state.messages.map(message => (
+        message.selected ? { ...message, read: false } : message
+      ))
+    })
+  }
+
+  deleteMessages() {
+    const messages = this.state.messages.filter(message => !message.selected)
+    this.setState({ messages })
+  }
+
+  toggleSelectAll() {
+    const selectedMessages = this.state.messages.filter(message => message.selected)
+    const selected = selectedMessages.length !== this.state.messages.length
+    this.setState({
+      messages: this.state.messages.map(message => (
+        message.selected !== selected ? { ...message, selected } : message
+      ))
+    })
+  }
+
+  applyLabel(label) {
+    const messages = this.state.messages.map(message => (
+      message.selected && !message.labels.includes(label) ?
+        { ...message, labels: [...message.labels, label].sort() } :
+        message
+    ))
+    this.setState({ messages })
+  }
+
+  removeLabel(label) {
+    const messages = this.state.messages.map(message => {
+      const index = message.labels.indexOf(label)
+      if (message.selected && index > -1) {
+        return {
+          ...message,
+          labels: [
+            ...message.labels.slice(0, index),
+            ...message.labels.slice(index + 1)
+          ]
+        }
+      }
+      return message
+    })
+    this.setState({ messages })
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="navbar navbar-default" role="navigation">
+          <div className="container">
+            <div className="navbar-header">
+              <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
+                <span className="sr-only">Toggle navigation</span>
+                <span className="icon-bar"></span>
+                <span className="icon-bar"></span>
+                <span className="icon-bar"></span>
+              </button>
+              <a className="navbar-brand" href="/">React Inbox</a>
+            </div>
+          </div>
+        </div>
+
+        <div className="container">
+          <Toolbar
+            messages={this.state.messages}
+            markAsRead={this.markAsRead.bind(this)}
+            markAsUnread={this.markAsUnread.bind(this)}
+            deleteMessages={this.deleteMessages.bind(this)}
+            toggleSelectAll={this.toggleSelectAll.bind(this)}
+            applyLabel={this.applyLabel.bind(this)}
+            removeLabel={this.removeLabel.bind(this)}
+            />
+          <Messages
+            messages={this.state.messages}
+            toggleSelect={this.toggleSelect.bind(this)}
+            toggleStar={this.toggleStar.bind(this)}
+            />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Messages.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import Message from './Message'
+
+const Messages = ({
+  messages,
+  toggleSelect,
+  toggleStar,
+}) => {
+  const messageComponents = messages.map(message => (
+    <Message
+      key={message.id}
+      message={message}
+      toggleSelect={toggleSelect}
+      toggleStar={toggleStar}
+      />
+  ))
+
+  return (
+    <div>
+      {messageComponents}
+    </div>
+  )
+}
+
+export default Messages
