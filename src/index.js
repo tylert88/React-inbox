ceacf99cbb35d9bf874437f630fc772bb964cfb6
Tyler Torres <tyler.john.torres@gmail.com>
2017-10-11 18:31:46 -0600

    Changed render settings to deploy to heroku

--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap-theme.css';
 import 'font-awesome/css/font-awesome.css';
 import './index.css';
-import App from './App';
+import MainApp from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<MainApp />, document.getElementById('root'));
 registerServiceWorker();
